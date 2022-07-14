758c5affcdffaeb99ec8b95f02ddfad2d68ed631
hellovolcano <valerie.gleason@gmail.com>
2022-07-14 10:23:31 -0500

    adding ID to notes so load functionality works

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,14 @@
 const router = require("express").Router()
 const { Router } = require("express")
 const notes = require("../../db/db")
-const createNewNote = require("../../lib/note")
+const { createNewNote, assignUniqueId } = require("../../lib/note")
 
 router.get("/notes", (req, res) => {
     res.json(notes)
 })
 
 router.post("/notes", (req, res) => {
-    // add an ID so we can eventually handle DELETE requests
-    // req.body.id = notes.length.toString()
-    const note = createNewNote(req.body, notes)
+    const note = createNewNote(assignUniqueId(req, notes), notes)
     res.json(note)
 
     
